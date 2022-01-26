602f96ad2dca12df65d156832595c114603061d1
masaya0411 <msya.0411@gmail.com>
2022-01-26 11:45:19 +0900

    ページネーション修正

--- resources/views/vendor/pagination/bootstrap-4.blade.php
+++ resources/views/vendor/pagination/bootstrap-4.blade.php
@@ -1,11 +1,7 @@
 @if ($paginator->hasPages())
     <ul class="c-pagination" role="navigation">
         {{-- Previous Page Link --}}
-        @if ($paginator->onFirstPage())
-            <li class="c-pagination__item" aria-disabled="true" aria-label="@lang('pagination.previous')">
-                <span class="c-pagination__link" aria-hidden="true">&lsaquo;</span>
-            </li>
-        @else
+        @if (!$paginator->onFirstPage())
             <li class="c-pagination__item">
                 <a class="c-pagination__link" href="{{ $paginator->previousPageUrl() }}" rel="prev" aria-label="@lang('pagination.previous')">&lsaquo;</a>
             </li>
@@ -34,10 +30,6 @@
         @if ($paginator->hasMorePages())
             <li class="c-pagination__item">
                 <a class="c-pagination__link" href="{{ $paginator->nextPageUrl() }}" rel="next" aria-label="@lang('pagination.next')">&rsaquo;</a>
-            </li>
-        @else
-            <li class="c-pagination__item" aria-disabled="true" aria-label="@lang('pagination.next')">
-                <span class="c-pagination__link" aria-hidden="true">&rsaquo;</span>
             </li>
         @endif
     </ul>
