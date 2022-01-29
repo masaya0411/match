8ff0aedc59ee56d63b18af80e38a590989612329
masaya0411 <msya.0411@gmail.com>
2022-01-29 22:15:09 +0900

    props型修正

--- public/js/app.js
+++ public/js/app.js
@@ -2122,7 +2122,7 @@ __webpack_require__.r(__webpack_exports__);
 //
 /* harmony default export */ __webpack_exports__["default"] = ({
   props: {
-    directBords: Object
+    directBords: Array
   },
   data: function data() {
     return {};
--- resources/js/components/DirectMessage.vue
+++ resources/js/components/DirectMessage.vue
@@ -35,7 +35,7 @@
 
     export default {
         props: {
-            directBords: Object, 
+            directBords: Array, 
         },
         data: function() {
             return {
