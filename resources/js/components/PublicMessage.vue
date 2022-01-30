438ff0b9f0aa52efa4155255f5aa9447bd4ef193
masaya0411 <msya.0411@gmail.com>
2022-01-30 10:28:49 +0900

    PublicMessageItem追加

--- resources/js/components/PublicMessage.vue
+++ resources/js/components/PublicMessage.vue
@@ -5,32 +5,19 @@
             メッセージはありません
         </div>
 
-        <li v-else v-for="publicMessage in publicMessages" :key="publicMessage.id" class="c-panel__item">
-            <a :href="'/products/' + publicMessage.id" class="c-panel__link">
-                <h3 class="c-panel__title">
-                    {{ publicMessage.title }}
-                </h3>
-                <div class="p-mypage__pb-msg__wrap">
-                    <p class="p-mypage__pb-msg__text">
-                        {{ publicMessage.msg.public_msg }}
-                    </p>
-                    <span class="p-mypage__pb-msg__time">
-                        {{ formatDate(publicMessage.msg.created_at) }}
-                    </span>
-                </div>
-            </a>
-        </li>
+        <PublicMessageItem v-else v-for="publicMessage in publicMessages" :key="publicMessage.id" :public-message="publicMessage"/>
+
     </ul>
 </template>
 
 <script>
     import moment from "moment"
-    // import PublicMessageItem from "./PublicMessageItem";
+    import PublicMessageItem from "./PublicMessageItem";
 
     export default {
-        // components: {
-        //     PublicMessageItem
-        // },
+        components: {
+            PublicMessageItem
+        },
         props: {
             publicMessages: null
         },
@@ -41,11 +28,6 @@
                 }else{
                     return false;
                 }
-            }
-        },
-        methods: {
-            formatDate(date) {
-                if(!!date) return moment(date).format("YYYY/MM/DD hh:mm");
             }
         }
     }
---
+++ resources/js/components/PublicMessageItem.vue
@@ -0,0 +1,34 @@
+<template>
+
+        <li class="c-panel__item">
+            <a :href="'/products/' + publicMessage.id" class="c-panel__link">
+                <h3 class="c-panel__title">
+                    {{ publicMessage.title }}
+                </h3>
+                <div class="p-mypage__pb-msg__wrap">
+                    <p class="p-mypage__pb-msg__text">
+                        {{ publicMessage.msg.public_msg }}
+                    </p>
+                    <span class="p-mypage__pb-msg__time">
+                        {{ formatDate(publicMessage.msg.created_at) }}
+                    </span>
+                </div>
+            </a>
+        </li>
+
+</template>
+
+<script>
+    import moment from "moment"
+
+    export default {
+        props: {
+            publicMessage: Object
+        },
+        methods: {
+            formatDate(date) {
+                if(!!date) return moment(date).format("YYYY/MM/DD hh:mm");
+            }
+        }
+    }
+</script>
